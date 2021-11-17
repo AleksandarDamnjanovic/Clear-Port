863f18ed6b83568874fc119647b0599939c6c251
AleksandarDamnjanovic <aor.aleksandar@gmail.com>
2021-11-17 19:16:33 +0100

    Updated

--- cp.r
+++ cp.r
@@ -7,7 +7,10 @@ if(length(args)==0){
 	stop()
 }
 
-c1<-paste("sudo netstat -tanopl|grep ", args[1])
+args<-c(44444)
+
+c1<-paste("sudo netstat -tanopl|grep", args[1])
+print(c1)
 
 content<-system(c1, intern = TRUE)
 splited<-strsplit(content, " ")
@@ -23,20 +26,23 @@ for(s in splited){
 for(s in full$source){
 	
 	pid<-full$pid[full$source==s]
+	print(paste('Detectec process id:', pid))
 	pid<-strsplit(pid,"/")
 	process<-pid[[1]][1]
+	print(process)
 	for(a in args){
 		value<-grepl(a, s, fixed=TRUE)
-
 		done<-FALSE
 
-		if(is.numeric(process)){
-			com<-paste("sudo kill -9 ",process)
+		if(is.numeric(as.numeric(process))){
+			com<-paste("sudo kill -9",process)
 			if(value==TRUE){
 				system(as.character(com))
-				print(paste("killed process id:", com))
+				print(com)
 				done<-TRUE
 			}
+		}else{
+			print("Error processing process id")
 		}
 
 		if(!done)
